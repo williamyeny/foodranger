8cffcec57c11018685906b6be05b4399d959d0f1
williamyeny <williamyeny@gmail.com>
2016-11-19 18:18:44 -0500

    favicon

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var express = require("express");
 var app = express();
-var favicon = require('serve-favicon');
+// var favicon = require('serve-favicon');
 
 var port = process.env.PORT || 8080;
 
 var games = {};
 
 app.use(express.static("public"));
-app.use(favicon(__dirname + '/favicon.ico'));
+// app.use(favicon(__dirname + '/favicon.ico'));
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res) {

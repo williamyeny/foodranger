39bf6b848710b354a2f1b940c331910104dc3870
williamyeny <williamyeny@gmail.com>
2016-11-19 20:05:28 -0500

    sending data over reg-res

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 // var favicon = require('serve-favicon');
 
 var port = process.env.PORT || 8080;
@@ -9,6 +10,7 @@ var games = {};
 app.use(express.static("public"));
 // app.use(favicon(__dirname + '/favicon.ico'));
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", function(req, res) {
 
@@ -17,6 +19,12 @@ app.get("/", function(req, res) {
 
 app.get("/reg-res", function(req, res) {
   res.render('reg-rest');
+})
+
+app.post("/submit-res", function(req, res) {
+  // console.log(req.body);
+  res.send(req.body.restName + " " + req.body.location);
+  // res.redirect("/");
 })
 
 
